d688e6cdbc1ed9ef3fb05db0e4f23611da86168d
Aanshikesh <sbrawat790@gmail.com>
2025-07-26 13:27:16 +0530

    Added build Script

--- Server/index.js
+++ Server/index.js
@@ -22,12 +22,7 @@ database.connect();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-	cors({
-		origin:"http://localhost:3000",
-		credentials:true,
-	})
- )
+app.use(cors());
 
 app.use(
 	fileUpload({
